87a8a4f122775bb2f3a27e5e8a33a888f8b6eec3
sungyong1234 <107783231+sungyong1234@users.noreply.github.com>
2023-01-12 12:59:40 +0900

    Update source.lua

--- source.lua
+++ source.lua
@@ -31,8 +31,8 @@ game:GetService("StarterGui"):SetCore(
 	{
 		Title = "Korea NP Hub",
 		Duration = 8,
-		Text = "NP Hub를 로딩하는 중입니다.\n\n디스코드 커뮤니티에 참가해주세요.",
-		Button1 = "Join Discord!",
+		Text = "오늘도 NP Hub를 이용해주셔서 감사합니다. \n더좋은 서비스로 보답하겠습니다!\n오늘도 NP Hub를 로딩하는중입니다.",
+		Button1 = "NP Hub Kakaotalk",
 		Callback = NotificationBindable
 	}
 )
